69dcf10abd4f8bc423e44a6cb891778b669b78a1
RangerDigital <36769448+RangerDigital@users.noreply.github.com>
2021-07-22 23:20:16 +0200

    Refactor Hello.vue

--- frontend/src/components/Hello.vue
+++ frontend/src/components/Hello.vue
@@ -1,7 +1,16 @@
 <template>
-  <section class="flex flex-col items-center justify-center w-screen h-screen">
-    <h1 class="text-3xl">Hello from Kuroki</h1>
-    <p class="my-5 text-lg">Served by CloudFlare Pages and {{ node }} Kubernetes node!</p>
+  <section class="flex flex-col items-center justify-center w-screen h-screen bg-black">
+    <h1 class="mx-1 text-6xl font-semibold text-center text-primary font-heading">Hello World! ~ Kuroki</h1>
+    <p class="mx-1 my-8 text-lg text-center text-white"
+      >This request was served by <span class="text-primary">Cloudflare Pages</span> and <span class="text-primary">Kubernetes</span> pod with the name
+      <span class="text-primary">{{ node }}</span
+      >.</p
+    >
+
+    <p class="max-w-2xl mx-1 my-2 text-xs text-center text-gray"
+      >Kuroki is built with Fastify and Vue.js (with Vite and TailwindCSS in JIT mode). Frontend is deployed to the Cloudflare global content delivery network. Backend requests are
+      handled by a bare-metal K3s cluster running on Hetzner Cloud, managed in GitOps way by FluxCD v2, on volatile infrastructure provisioned by Ansible.
+    </p>
   </section>
 </template>
 
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,10 +3,30 @@ module.exports = {
   purge: ['./public/**/*.html', './src/**/*.{js,jsx,ts,tsx,vue}'],
   darkMode: false,
   theme: {
-    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      white: {
+        DEFAULT: '#F3F3F4',
+      },
+      primary: {
+        DEFAULT: '#F94943',
+      },
+      gray: {
+        DEFAULT: '#7f7f7f',
+      },
+      black: {
+        DEFAULT: '#151414',
+      },
+    },
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+      heading: ['Poppins', 'sans-serif'],
+    },
+    extend: {
+      screens: {
+        '3xl': '2000px',
+      },
+    },
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
 };
