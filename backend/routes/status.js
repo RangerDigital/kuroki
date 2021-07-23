1236d06f59821ec0a7d270bf731d936b6e54c2ed
RangerDigital <36769448+RangerDigital@users.noreply.github.com>
2021-07-23 10:10:40 +0200

    Update versioning

--- backend/routes/status.js
+++ backend/routes/status.js
@@ -23,7 +23,7 @@ async function routes(router) {
     },
     async (req, res) => {
       const time = Date.now();
-      const version = '0.3';
+      const version = '0.4';
 
       return res.send({ name: name, time: time, version: version });
     }
