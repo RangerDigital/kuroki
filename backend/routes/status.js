6160890b7fb054bf962355a36faee39e03cf94d1
RangerDigital <36769448+RangerDigital@users.noreply.github.com>
2021-07-23 11:44:54 +0200

    Bump version

--- backend/routes/status.js
+++ backend/routes/status.js
@@ -23,7 +23,7 @@ async function routes(router) {
     },
     async (req, res) => {
       const time = Date.now();
-      const version = '0.6';
+      const version = '0.7';
 
       return res.send({ name: name, time: time, version: version });
     }
--- frontend/src/components/Hello.vue
+++ frontend/src/components/Hello.vue
@@ -3,7 +3,7 @@
     <h1 class="mx-1 text-6xl font-semibold text-center text-primary font-heading">Hello World! ~ Kuroki</h1>
     <p class="mx-1 my-8 text-lg text-center text-white"
       >This request was served by <span class="text-primary">Cloudflare Pages</span> and <span class="text-primary">Kubernetes</span> pod with the name
-      <span class="text-primary">{{ node }}</span
+      <span class="text-primary">{{ node }} v.{{ version }}</span
       >.</p
     >
 
@@ -20,6 +20,7 @@
     data() {
       return {
         node: 'Unknown',
+        version: '0.0',
       };
     },
     methods: {
@@ -28,6 +29,7 @@
           .get('/status')
           .then((payload) => {
             this.node = payload.data.name;
+            this.version = payload.data.version;
           })
           .catch((err) => {
             console.log(err);
