193c85353447e8bc0c76ed6561c08af47fc7addb
RangerDigital <36769448+RangerDigital@users.noreply.github.com>
2021-07-26 21:27:14 +0200

    Update text

--- frontend/src/components/Hello.vue
+++ frontend/src/components/Hello.vue
@@ -1,16 +1,10 @@
 <template>
   <section class="flex flex-col items-center justify-center w-screen h-screen bg-black">
-    <h1 class="mx-1 text-6xl font-semibold text-center text-primary font-heading">Hello World! ~ Kuroki</h1>
-    <p class="mx-1 my-8 text-lg text-center text-white"
-      >This request was served by <span class="text-primary">Cloudflare Pages</span> and <span class="text-primary">Kubernetes</span> pod with the name
-      <span class="text-primary">{{ node }} v{{ version }}</span
-      >.</p
+    <h1 class="mx-1 text-6xl font-semibold text-center 3xl:text-8xl text-primary font-heading">Hello, I'm Kuroki!</h1>
+    <p class="mx-1 my-8 text-lg text-center text-white 3xl:text-xl"
+      >This request was served by <span class="text-primary">Cloudflare Pages</span> and my <span class="text-primary">Kubernetes</span> cluster. (🦇 From
+      <span class="text-primary">{{ node }} v{{ version }}</span> container.)</p
     >
-
-    <p class="max-w-2xl mx-1 my-2 text-xs text-center text-gray"
-      >Kuroki is built with Fastify and Vue.js (with Vite and TailwindCSS in JIT mode). Frontend is deployed to the Cloudflare global content delivery network. Backend requests are
-      handled by a bare-metal K3s cluster running on Hetzner Cloud, managed in GitOps way by FluxCD v2, on volatile infrastructure provisioned by Ansible.
-    </p>
   </section>
 </template>
 
